45831709b593e3fc393e6122aa8932b0a9324a86
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-07-29 09:15:26 +0200

    CREATE - (DB) : Create migrations + models product

--- back/products/models.py
+++ back/products/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
 
+class Category(models.Model):
+    categoryId = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=35)
+    slug = models.SlugField()
+
+
 class Product(models.Model):
     productId = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=35)
     slug = models.SlugField()
     price = models.FloatField()
-    plateform = models.CharField()
+    plateform = models.CharField(max_length=35)
     productDescription = models.TextField()
     createdDate = models.DateField()
+    category = models.ManyToManyField(Category)
     # Ajouter un champ pour les users quand la partie user sera opé
-
-
-class Category(models.Model):
-    title = models.CharField()
