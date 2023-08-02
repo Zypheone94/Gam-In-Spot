0d3d80cfc1d02e4a07abd94397d2a3922ff36ce5
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-08-02 15:07:43 +0200

    EVO - (Products) : Changes models & admin
    Changes somes models value for null & create function to create slug, adding some datas in db
    Begun admin display_list

--- back/products/admin.py
+++ back/products/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+    ]
+
+    readonly_fields = [
+        'slug',
+        'createdDate',
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+    ]
+
+    readonly_fields = [
+        'slug',
+    ]
--- back/users/admin.py
+++ back/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import CustomUser
 
-#admin.site.register(CustomUser)
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
