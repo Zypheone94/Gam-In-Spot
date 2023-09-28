d86c086aa6fa3231de546041810a82d6bd6d272b
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-09-28 14:59:28 +0200

    EVO - (CreateUserForm) : Récupération des données dans un state + vérif password

--- front/src/components/forms/CreateUserForm.jsx
+++ front/src/components/forms/CreateUserForm.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const CreateUserForm = () => {
 
+    const [formData, setFormData] = useState('');
+    const [returnError, setReturnError] = useState('')
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
+        console.log(formData)
+    };
+
+    const handleSubmit = (e) => {
+        if (formData.password === formData.verify_password) {
+            e.preventDefault()
+            console.log('verification database nécessaire puis création compte')
+        } else {
+            e.preventDefault()
+            setReturnError('Les deux mots de passes ne sont pas identiques')
+        }
+
+    }
 
     return (
         <>
-            <form className="mb-10">
+            <form className="mb-10" onSubmit={handleSubmit}>
                 <div className="flex flex-wrap">
                     <div className="mt-6 w-full flex justify-between">
                         <label className="w-2/5">Nom</label>
@@ -14,7 +33,10 @@ const CreateUserForm = () => {
                                style={{
                                    border: '1px solid #F72585',
                                    borderRadius: '10px'
-                               }}/>
+                               }}
+                               name="last_name"
+                               onChange={handleInputChange}
+                               required/>
                     </div>
                     <div className="mt-6 w-full flex justify-between">
                         <label className="w-2/5">Prénom</label>
@@ -23,7 +45,10 @@ const CreateUserForm = () => {
                                style={{
                                    border: '1px solid #F72585',
                                    borderRadius: '10px'
-                               }}/>
+                               }}
+                               name="first_name"
+                               onChange={handleInputChange}
+                               required/>
                     </div>
                 </div>
                 <div className="mt-6 w-full flex justify-between">
@@ -33,7 +58,10 @@ const CreateUserForm = () => {
                            style={{
                                border: '1px solid #F72585',
                                borderRadius: '10px'
-                           }}/>
+                           }}
+                           name="email"
+                           onChange={handleInputChange}
+                           required/>
                 </div>
                 <div className="mt-6 w-full flex justify-between">
                     <label className="w-2/5">Pseudo</label>
@@ -42,7 +70,10 @@ const CreateUserForm = () => {
                            style={{
                                border: '1px solid #F72585',
                                borderRadius: '10px'
-                           }}/>
+                           }}
+                           name="username"
+                           onChange={handleInputChange}
+                           required/>
                 </div>
                 <div className="mt-6 w-full flex justify-between">
                     <label className="w-2/5">Date de naissance</label>
@@ -51,7 +82,10 @@ const CreateUserForm = () => {
                            style={{
                                border: '1px solid #F72585',
                                borderRadius: '10px'
-                           }}/>
+                           }}
+                           name="birth_date"
+                           onChange={handleInputChange}
+                           required/>
                 </div>
                 <div className="flex flex-wrap">
                     <div className="mt-6 w-full flex justify-between">
@@ -61,7 +95,10 @@ const CreateUserForm = () => {
                                style={{
                                    border: '1px solid #F72585',
                                    borderRadius: '10px'
-                               }}/>
+                               }}
+                               name="password"
+                               onChange={handleInputChange}
+                               required/>
                     </div>
                     <div className="mt-6 w-full flex justify-between">
                         <label className="w-2/5">Confirmation</label>
@@ -69,9 +106,19 @@ const CreateUserForm = () => {
                                style={{
                                    border: '1px solid #F72585',
                                    borderRadius: '10px'
-                               }}/>
+                               }}
+                               name="verify_password"
+                               onChange={handleInputChange}
+                               required/>
                     </div>
                 </div>
+                {
+                    returnError ? (
+                        <p style={{color: 'red', marginTop: '20px'}}>{returnError}</p>
+                    ) : (
+                        <></>
+                    )
+                }
                 <div className='w-full mt-6 flex justify-end'>
                     <button type="submit">
                         Créer le compte
