2d1f4cd6e8981fef6b92f358e2268885ed7cc17c
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-10-06 11:34:26 +0200

    Encore test url api

--- front/src/utils/api.jsx
+++ front/src/utils/api.jsx
@@ -13,9 +13,10 @@ export const api = async (apiDetailRoad, method = 'GET', data = {}) => {
         // Inclure le corps de requête uniquement si ce n'est pas une requête GET
         requestOptions.body = JSON.stringify(data);
     }
+    console.log('apiDetailRoad:', apiDetailRoad);
 
     try {
-        const response = await fetch(`http://46.101.3.222:8000/${apiDetailRoad}`, requestOptions);
+        const response = await fetch(`http://46.101.3.222:8000${apiDetailRoad}`, requestOptions);
         return await response.json();
     } catch (error) {
         throw error;
