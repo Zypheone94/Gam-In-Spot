0b96fc843be8df2d5d36c571653aab2f955dcde1
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-10-06 13:51:25 +0200

    Ajout secu HTTPS back

--- back/Gaminspot/settings.py
+++ back/Gaminspot/settings.py
@@ -152,3 +152,15 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
+
+SECURE_SSL_REDIRECT = True
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_BROWSER_XSS_FILTER = True
