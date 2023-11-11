b0e0682a27fc623c976757959ba5d4289c0e91bf
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-11-11 22:40:12 +0100

    EVO - (Historique) : Récupération de tout les articles d'un utilisateur puis affichage sous form de card dans la page historique

--- front/src/pages/user/HistoryUser.jsx
+++ front/src/pages/user/HistoryUser.jsx
@@ -1,8 +1,56 @@
+import React, {useEffect, useState} from "react";
+import {useSelector} from "react-redux"
+
+import {api} from "../../utils/api.jsx";
+import {useNavigate} from "react-router-dom";
+
+import ProductCard from "../../components/commons/product/ProductCard.jsx";
+
 const HistoryUser = () => {
+
+    const user = useSelector(state => state.user)
+    const navigate = useNavigate()
+
+    const [productList, setProductList] = useState()
+
+    useEffect(() => {
+        if (user === null || user.email === undefined) {
+            navigate('/login')
+        } else {
+            getAllUserProducts()
+            console.log(productList)
+        }
+    }, [])
+
+    const getAllUserProducts = async () => {
+        try {
+            const response = await api('products/product/loadProductList', 'POST', {'seller_id': user.id});
+            setProductList(response)
+        } catch (error) {
+            console.error('Error fetching product list:', error);
+        }
+    }
 
 
     return (
-        <>Page historique</>
+        <>
+            <h3 className='text-pink'>historique</h3>
+            <div className='flex flex-wrap'>
+                {
+                    productList && productList.length > 0 ?
+                        productList.map((product, index) =>
+
+                            (
+                                <ProductCard productValue={product}/>
+                            )
+                        ) :
+                        <p>
+                            Vous n'avez pas encore de produit mis en ligne
+                        </p>
+                }
+            </div>
+        </>
+
     )
 }
 
