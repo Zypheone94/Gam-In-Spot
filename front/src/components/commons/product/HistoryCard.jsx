014b6f3e0a9e2de51b84c2c535b929b6f6ec08c1
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-11-13 15:30:45 +0100

    EVO - (Historique) : Responsive de la liste historique, à faire : ajouter la suppression

--- front/src/components/commons/product/HistoryCard.jsx
+++ front/src/components/commons/product/HistoryCard.jsx
@@ -8,37 +8,32 @@ const HistoryCard = ({productValue}) => {
     const navigate = useNavigate()
 
     return (
-        <div className='mx-4 w-full mt-6' style={{
+        <div className='mx-4 w-full mt-6 h-20 lg:h-32' style={{
             border: '1px solid #4361EE',
             borderRadius: '20px',
             cursor: 'pointer',
-            height: '80px',
         }} onClick={() => navigate('/product/' + productValue.slug)}>
 
             <div className='text-lightPurple flex h-full'>
-                <div className='w-1/4' style={{
+                <div className='w-1/4 md:w-3/5' style={{
                     clipPath: 'polygon(0px 0px, -0.63% 196.25%, 100% 0px)',
-
-
                 }}>
                     <div className='w-full h-full'
-                        style={{
-                            borderRadius: '20px 0 20px 20px',
-                            backgroundPosition: 'center',
-                            backgroundSize: 'cover',
-                            backgroundImage: productValue.images ? `url(${productValue.images[0]})` : `url(https://i0.wp.com/leszackardises.com/wp-content/uploads/2020/09/D54E7AD0-1B8A-48F6-84E2-BDD90258F445.jpeg?w=1000&ssl=1)`
-                        }}
-
-                    ></div>
+                         style={{
+                             borderRadius: '20px 0 20px 20px',
+                             backgroundPosition: 'center',
+                             backgroundSize: 'cover',
+                             backgroundImage: productValue.images ? `url(${productValue.images[0]})` : `url(https://i0.wp.com/leszackardises.com/wp-content/uploads/2020/09/D54E7AD0-1B8A-48F6-84E2-BDD90258F445.jpeg?w=1000&ssl=1)`
+                         }}>
+                    </div>
                 </div>
 
+
                 <div className='flex justify-between items-center w-full p-2 lg:ml-16'>
-                    <p className='font-semibold text-deepPurple flex-1'>{nommage(productValue.title, 12)}</p>
-                    <p className='font-semibold text-deepPurple flex-1'>{productValue.price}€</p>
-                    <p className='flex-1'>
-                        De: {nommage(productValue.seller, 15)}
-                    </p>
-                    <p className='flex-1'>
+                    <p className='hidden md:block font-semibold text-deepPurple flex-1 text-center'>{nommage(productValue.title, 18)}</p>
+                    <p className='md:hidden font-semibold text-deepPurple flex-1 text-center'>{nommage(productValue.title, 12)}</p>
+                    <p className='font-semibold text-deepPurple flex-1 text-center'>{productValue.price}€</p>
+                    <p className='flex-1 text-center'>
                         Mise en ligne : <DateFormat value={productValue.createdDate}/>
                     </p>
                 </div>
