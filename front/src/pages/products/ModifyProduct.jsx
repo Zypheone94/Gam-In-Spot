1bcef09291c1921f71528f783b50987e503e27d0
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-11-14 11:29:34 +0100

    EVO - (ModifyProduct) : Création d'un modifiy form + attribution des données chargées à chaque champ

--- front/src/pages/products/ModifyProduct.jsx
+++ front/src/pages/products/ModifyProduct.jsx
@@ -1,17 +1,40 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useLocation} from 'react-router-dom'
 
 import {api} from "../../utils/api.jsx";
+import WrongPage from "../WrongPage.jsx";
+
+import ModifyProductForm from "../../components/forms/ModifyProductForm.jsx";
 
 const ModifyProduct = () => {
+
+    const [productDetail, setProductDetail] = useState(null)
+    const [display404, setDisplay404] = useState(false)
+    const [loading, setLoading] = useState(true)
+
 
     const locate = useLocation().pathname
 
     useEffect(() => {
         const slug = locate.split('/').slice(3).join('/')
-        console.log(slug)
-        changeProductData(slug)
+        getProductData(slug)
+        // changeProductData(slug)
+        console.log(productDetail)
+
     }, []);
+
+    const getProductData = async (slug) => {
+        try {
+            const response = await api(`products/read-product/${slug}`);
+            setProductDetail(response);
+            if (response.error === 100) {
+                setDisplay404(true)
+            }
+            setLoading(false);
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données:', error);
+        }
+    }
 
     const changeProductData = async (slug) => {
         try {
@@ -23,10 +46,19 @@ const ModifyProduct = () => {
     };
 
     return (
-        <>
-            Modify product page
-        </>
-    )
+        <section>
+            {loading ? (
+                <p>Chargement...</p>
+            ) : (
+
+                display404 === true ? (
+                    <WrongPage/>
+                ) : (
+                    <ModifyProductForm productDetail={productDetail} setProductDetail={setProductDetail}/>
+                )
+
+            )}
+        </section>)
 }
 
 export default ModifyProduct
