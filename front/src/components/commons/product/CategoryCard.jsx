b7801c004feacce7f8e18ddb2b63f9615a2e8eef
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-11-23 10:32:28 +0100

    EVO - (Category Card) : Ajout d'une petite animation de hover lors du passage de la souris sur la card

--- front/src/components/commons/product/CategoryCard.jsx
+++ front/src/components/commons/product/CategoryCard.jsx
@@ -6,6 +6,8 @@ import DateFormat from "../../../utils/DateFormat.jsx";
 
 const CategoryCard = ({categorySlug, categoryTitle}) => {
 
+    const navigate = useNavigate()
+
     const [imageExists, setImageExists] = useState()
 
     const checkImageExist = (categorie) => {
@@ -21,7 +23,15 @@ const CategoryCard = ({categorySlug, categoryTitle}) => {
         };
     }
 
-    const navigate = useNavigate()
+    const changeBackgroundColor = (element) => {
+        element.style.backgroundColor = 'rgba(0,0,0,0)';
+        element.children[0].style.opacity = 0;
+    };
+
+    const restoreBackgroundColor = (element) => {
+        element.style.backgroundColor = 'rgba(0,0,0,0.6)';
+        element.children[0].style.opacity = 1;
+    };
 
     useEffect(() => {
         checkImageExist(categoryTitle)
@@ -40,14 +50,25 @@ const CategoryCard = ({categorySlug, categoryTitle}) => {
         }} onClick={() => navigate('/category/' + categorySlug)}>
             <div style={{
                 backgroundColor: 'rgba(0,0,0,0.6)',
-
                 height: '100%',
                 width: '100%',
                 borderRadius: '20px',
-            }}>
-
+                transition: 'background-color 0.5s ease',
+                position: 'relative',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                opacity: 1,
+            }}
+                 onMouseEnter={(e) => changeBackgroundColor(e.target)}
+                 onMouseLeave={(e) => restoreBackgroundColor(e.target)}>
+                <p style={{
+                    color: 'white',
+                    opacity: 1,
+                    transition: 'opacity 0.4s ease',
+                }}>{categoryTitle}</p>
             </div>
-            {categoryTitle}
+
         </div>
     )
 }
