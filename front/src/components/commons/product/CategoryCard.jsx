926449a4fead095d04b2f239270a73c31cc9c402
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-11-23 08:59:18 +0100

    EVO - (Category Card) : Création d'une fonction qui check l'existance d'une image liée à une catégory et la retourne si c'est le cas

--- front/src/components/commons/product/CategoryCard.jsx
+++ front/src/components/commons/product/CategoryCard.jsx
@@ -1,10 +1,31 @@
+import {useEffect, useState} from "react";
+
 import {useNavigate} from "react-router-dom";
 import {nommage} from "../../../utils/nommage.jsx";
 import DateFormat from "../../../utils/DateFormat.jsx";
 
 const CategoryCard = ({categorySlug, categoryTitle}) => {
 
+    const [imageExists, setImageExists] = useState()
+
+    const checkImageExist = (categorie) => {
+        const img = new Image();
+        img.src = `../../src/assets/images/category/${categorie}.jpg`;
+        img.onload = () => {
+            setImageExists(img.src);
+            console.log('ok')
+        };
+        img.onerror = () => {
+            setImageExists(false);
+            console.log('nope')
+        };
+    }
+
     const navigate = useNavigate()
+
+    useEffect(() => {
+        checkImageExist(categoryTitle)
+    }, [])
 
     return (
         <div className='mx-8 my-4' style={{
@@ -15,6 +36,7 @@ const CategoryCard = ({categorySlug, categoryTitle}) => {
             cursor: 'pointer',
         }} onClick={() => navigate('/category/' + categorySlug)}>
             {categoryTitle}
+            <img src={imageExists}/>
         </div>
     )
 }
