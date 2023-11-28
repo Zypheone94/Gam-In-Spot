b811559b42d288b9f9bd4317ed92de6323e09487
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-11-28 16:35:31 +0100

    EVO - (Recherche Catégorie) : Mise en place de la première recherche celle des catégories

--- front/src/pages/Search.jsx
+++ front/src/pages/Search.jsx
@@ -1,11 +1,35 @@
 import {useEffect, useState} from "react";
+import {api} from "../utils/api.jsx";
 
 import {useLocation} from 'react-router-dom';
+
+import CategoryCard from "../components/commons/product/CategoryCard.jsx";
 
 const Search = () => {
 
     const location = useLocation()
+    const [loading, setLoading] = useState(true)
     const [searchValue, setSearchValue] = useState('')
+    const [categoryList, setCategoryList] = useState()
+
+    useEffect(() => {
+        console.log(searchValue)
+        const getData = async () => {
+            try {
+                setLoading(true)
+                let url = `/products/api/category/`
+                const response = await api(url);
+                console.log(response)
+                let orderValue = response.sort()
+                setCategoryList(orderValue);
+                setLoading(false);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données:', error);
+            }
+        };
+
+        getData();
+    }, []);
 
     useEffect(() => {
         setSearchValue(location.search.substring(2))
@@ -18,7 +42,17 @@ const Search = () => {
 
                 <div>
                     <h3 className='text-pink mt-2 ml-8 mb-10'>Recherche par Catégorie</h3>
-                    ici la liste des categories contenu dans la recherche
+                    <div className='flex flex-wrap justify-start'>
+                        {
+                            categoryList && categoryList.map((cat) => {
+                                return cat.title.toLowerCase().includes(searchValue.toLowerCase()) && (
+                                    <div className='flex justify-center w-1/2 md:w-1/3 lg:w-1/5'>
+                                        <CategoryCard key={cat.slug} categoryTitle={cat.title} categorySlug={cat.slug}/>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
                 </div>
 
                 <div>
