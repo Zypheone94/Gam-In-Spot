b650a5ef73bbf13e10b6a84aba4f7a22aca48e58
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-11-28 15:38:47 +0100

    EVO - (Search) : Creation du component et de la route pour la recherche

--- front/src/routes/MainRoutes.jsx
+++ front/src/routes/MainRoutes.jsx
@@ -1,6 +1,7 @@
 // import Pages
 import Homepage from "../pages/Homepage.jsx";
 import WrongPage from "../pages/WrongPage.jsx";
+import Search from "../pages/Search.jsx";
 
 // Import Components
 import Header from "../components/commons/header/Header.jsx";
@@ -12,6 +13,10 @@ const MainRoutes = [
         element: <> <Header/><Homepage/><Footer/> </>,
         errorElement: <> <Header/><WrongPage/> <Footer/> </>
     },
+    {
+        path: "/search",
+        element: <> <Header/><Search/><Footer/></>
+    }
 ]
 
 export default MainRoutes
