26e58bd7dca833e5634fc409918de1a5cea97760
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-12-05 12:32:55 +0100

    EVO - (Panier) : Ajout de l'app panier à mon projet + création du model panier et migration en BDD

--- back/cart/models.py
+++ back/cart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import CustomUser
+from products.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+class CartElement(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
