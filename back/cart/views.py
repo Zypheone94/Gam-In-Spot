d685e5dc74f8bd7d72c836edca0a9e738ad93e5e
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-12-12 09:50:22 +0100

    EVO - (Cart) : Récupération de la route en front

--- back/cart/views.py
+++ back/cart/views.py
@@ -7,21 +7,20 @@ from .serializer import CartSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class CartView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, user_id):
+    def get(self, request):
         try:
+            user_id = request.GET.get('user_id')
             user = CustomUser.objects.get(id=user_id)
             cart = Cart.objects.filter(user=user).first()
             if cart:
-                serializer = CartSerializer(cart) 
+                serializer = CartSerializer(cart)
                 return Response(serializer.data, status=status.HTTP_200_OK)
             else:
-                # Si le panier n'existe pas, appel de l'endpoint de création du panier
-                create_cart_view = CreateCartView.as_view()
-                return create_cart_view(request._request, user_id).render()
+                # Si le panier n'existe pas, tu peux choisir de renvoyer un message spécifique plutôt que de rappeler la création du panier
+                return Response({"message": "Cart does not exist for this user"}, status=status.HTTP_404_NOT_FOUND)
         except CustomUser.DoesNotExist:
             return Response({"message": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
 
 class CreateCartView(APIView):
     def post(self, request, user_id):
--- front/src/pages/cart/Cart.jsx
+++ front/src/pages/cart/Cart.jsx
@@ -1,10 +1,35 @@
+import {useState, useEffect} from "react";
+import {api} from "../../utils/api.jsx";
+import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+
 const Cart = () => {
+
+    const user = useSelector(state => state.user)
+    const navigate = useNavigate()
+
+    const getUserCart = async () => {
+        try {
+            const request = await api('cart/?user_id=' + user.id, "GET")
+            console.log(request.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        if (user === null || user.email === undefined) {
+            navigate('/login')
+        } else {
+            getUserCart()
+        }
+    }, []);
 
     return (
         <>
-        <div>
-            Cart
-        </div>
+            <div>
+                Cart
+            </div>
         </>
     )
 }
