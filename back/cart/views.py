ab1d02f31c571b0d127d5dd7b20d324eae4ace05
Zypheone94 <maximerene.durand.pro@gmail.com>
2023-12-12 11:38:21 +0100

    EVO - (Cart) : Création / récupération d'un panier en front lorsque l'on va sur la page concerné
    J'ai supprimé la requête de création vue qu'elle ne servait plus à rien

--- back/cart/views.py
+++ back/cart/views.py
@@ -16,17 +16,8 @@ class CartView(APIView):
                 serializer = CartSerializer(cart)
                 return Response(serializer.data, status=status.HTTP_200_OK)
             else:
-                # Si le panier n'existe pas, tu peux choisir de renvoyer un message spécifique plutôt que de rappeler la création du panier
-                return Response({"message": "Cart does not exist for this user"}, status=status.HTTP_404_NOT_FOUND)
-        except CustomUser.DoesNotExist:
-            return Response({"message": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
-
-
-class CreateCartView(APIView):
-    def post(self, request, user_id):
-        try:
-            user = CustomUser.objects.get(id=user_id)
-            cart = Cart.objects.create(user=user)
-            return Response({"message": "Cart created successfully"}, status=status.HTTP_201_CREATED)
+                new_cart = Cart.objects.create(user=user)
+                new_cart_serializer = CartSerializer(new_cart)
+                return Response(new_cart_serializer.data, status=status.HTTP_201_CREATED)
         except CustomUser.DoesNotExist:
             return Response({"message": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
