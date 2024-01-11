3e7fa40d62bb9b94fc2978d6260d9ec371c7b43d
Zypheone94 <maximerene.durand.pro@gmail.com>
2024-01-11 16:13:08 +0100

    EVO - (Legales) : Ajout des mentions légales + conditions d'utilisation

--- front/src/routes/MainRoutes.jsx
+++ front/src/routes/MainRoutes.jsx
@@ -3,6 +3,9 @@ import Homepage from "../pages/Homepage.jsx";
 import WrongPage from "../pages/WrongPage.jsx";
 import Search from "../pages/Search.jsx";
 
+import Legales from "../pages/legal/Legales.jsx";
+import Conditions from "../pages/legal/Conditions.jsx";
+
 // Import Components
 import Header from "../components/commons/header/Header.jsx";
 import Footer from "../components/commons/footer/Footer.jsx";
@@ -16,6 +19,14 @@ const MainRoutes = [
     {
         path: "/search",
         element: <> <Header/><Search/><Footer/></>
+    },
+    {
+        path: '/legales',
+        element: <> <Header/><Legales /><Footer/></>
+    },
+    {
+        path: '/conditions',
+        element: <> <Header/><Conditions /><Footer/></>
     }
 ]
 
