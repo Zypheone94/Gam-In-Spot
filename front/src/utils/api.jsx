4d23727abaaf337c910865702f6300f60e5dd25c
Zypheone94 <maximerene.durand.pro@gmail.com>
2024-02-29 14:10:22 +0100

    EVO - (APi) : chagnges

--- front/src/utils/api.jsx
+++ front/src/utils/api.jsx
@@ -15,7 +15,7 @@ export const api = async (apiDetailRoad, method = 'GET', data = {}) => {
     }
 
     try {
-        const response = await fetch(`${apiUrl}/${apiDetailRoad}`, requestOptions);
+        const response = await fetch(`${"http://gaminspot.games/gam-in-spot/back"≤}/${apiDetailRoad}`, requestOptions);
         return await response.json();
     } catch (error) {
         throw error;
