ae34ab4047b6484e85a6b279c2946fc9eab5068c
Zypheone94 <maximerene.durand.pro@gmail.com>
2024-03-11 13:12:14 +0100

    FIX CORS

--- back/Gaminspot/settings.py
+++ back/Gaminspot/settings.py
@@ -147,7 +147,7 @@ AUTHENTICATION_BACKENDS = ['Gaminspot.backend.EmailBackend', 'django.contrib.aut
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
+CORS_ALLOWED_ORIGINS = ['http://gaminspot.games', 'https://gaminspot',]
 
 SIMPLEREST_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
