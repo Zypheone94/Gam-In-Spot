a62a32b94ba01ac74ca9adbe30683d6616ee4452
Zypheone94 <maximerene.durand.pro@gmail.com>
2024-03-13 23:23:10 +0100

    fix

--- back/Gaminspot/settings.py
+++ back/Gaminspot/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/app/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'products', 'static'),
