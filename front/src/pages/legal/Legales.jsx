91813528e9b21bf852cea91e74f34248e8138bd2
Zypheone94 <maximerene.durand.pro@gmail.com>
2024-03-14 15:48:17 +0100

    ADD - Mention legales

--- front/src/pages/legal/Legales.jsx
+++ front/src/pages/legal/Legales.jsx
@@ -9,11 +9,8 @@ const Legales = () => {
             <p className='my-2'><u>Siège social :</u> Thiais 94320</p>
             <p className='my-2'><u>Numéro de téléphone :</u> 06.XX.XX.XX.XX</p>
             <p className='my-2'><u>Adresse e-mail :</u> contact@gaminspot.fr</p>
-            <p className='my-2'><u>Numéro d'identification (SIRET) :</u> [Numéro SIRET]</p>
-            <p className='my-2'><u>Capital social : </u>[Montant du capital social]</p>
 
-            <p><u>Hébergeur du site :</u> [Nom de l'hébergeur]</p>
-            <p><u>Adresse de l'hébergeur :</u> [Adresse de l'hébergeur]</p>
+            <p><u>Hébergeur du site :</u> Google Cloud Plateform</p>
 
             <h2 className='text-pink font-bold text-2xl my-4'>Propriété intellectuelle :</h2>
 
@@ -24,7 +21,7 @@ const Legales = () => {
             <h2 className='text-pink font-bold text-2xl my-4'>Protection des données personnelles :</h2>
 
             <p>Les informations collectées via ce site sont utilisées dans le cadre des activités de Gam'In-Spot. Conformément à la loi sur la protection des données, vous disposez d'un droit d'accès, de
-            rectification et de suppression de vos données en nous contactant à [adresse e-mail de contact].</p>
+            rectification et de suppression de vos données en nous contactant à admin@gaminspot.games.</p>
 
             <h2 className='text-pink font-bold text-2xl my-4'>Limitation de responsabilité :</h2>
 
