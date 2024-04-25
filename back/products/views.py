92f71200e737ea71398c7539721503476c2821c3
Stracker-Maxime <maxime@stracker.tech>
2024-04-25 14:08:39 +0200

    EVO - (images) : Retrait des images qui faisait bugger le code, ajout d'un placeholder

--- back/products/views.py
+++ back/products/views.py
@@ -76,21 +76,19 @@ class ProductDetailView(APIView):
             data = serializer.data
             data['seller'] = seller.__str__()
 
-            images = []
+            """images = []
 
             for i in range(1, 4):
-                url = f"https://gaminspot.games:8000/static/{data['seller']}/{data['slug']}/image_{i}.jpg"
-                print(url)
-                try:
-                    if check_url_status(url):
-                        images.append(url)
-                        return  # Quitte la boucle et la fonction si une URL valide est trouvée
-                except Exception as e:
-                    pass  # Passe à l'itération suivante sans lever d'erreur
+                url = f"https://localhost:8000/static/{data['seller']}/{data['slug']}/image_{i}.jpg"
+                if check_url_status(url):
+                    images.append(url)
+                else:
+                    None
 
-            # Si aucune URL valide n'a été trouvée, aucune action n'est nécessaire
-            if len(images) > 0:
-                data['images'] = images
+            if len(images) == 0:
+                None
+            else:
+                data['images'] = images"""
 
             return Response(data)
         except Product.DoesNotExist:
@@ -245,15 +243,15 @@ class LiteProductListView(APIView):
                 seller = CustomUser.objects.get(pk=serialized_product['seller_id'])
                 serialized_product['seller'] = seller.__str__()
 
-                images = []
+                """images = []
 
                 for i in range(1, 4):
-                    url = f"https://gaminspot.games:8000/static/{serialized_product['seller']}/{serialized_product['slug']}/image_{i}.jpg"
+                    url = f"http://localhost:8000/static/{serialized_product['seller']}/{serialized_product['slug']}/image_{i}.jpg"
                     if check_url_status(url):
                         images.append(url)
 
                 if images:
-                    serialized_product['images'] = images
+                    serialized_product['images'] = images"""
 
                 result_data.append(serialized_product)
 
