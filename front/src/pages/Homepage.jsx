b246a2c7a416a45487168d658166e8ed8edc7c16
Stracker-Maxime <maxime@stracker.tech>
2024-05-14 10:57:10 +0200

    EVO - (SMO) : Changement de l'import de l'image

--- front/src/pages/Homepage.jsx
+++ front/src/pages/Homepage.jsx
@@ -9,6 +9,8 @@ import CategoryCard from "../components/commons/product/CategoryCard.jsx";
 
 import {useNavigate} from "react-router-dom"
 import {Helmet} from "react-helmet";
+import Main from '../assets/images/banner/main.jpg'
+
 
 function Homepage() {
 
@@ -81,7 +83,7 @@ function Homepage() {
                 <meta property="og:description" content="Browse our e-commerce website for an extensive collection of video
                 games. Find the perfect game on our product page, featuring top-rated titles and unbeatable deals. About
                 this game"/>
-                <meta property="og:image" content="../../../assets/images/banner/main.jpg"/>
+                <meta property="og:image" content={Main}/>
                 <meta property="og:url" content="https://gaminspot.games"/>
                 <meta property="og:type" content="website"/>
                 <meta name="twitter:card" content="summary_large_image"/>
@@ -95,7 +97,7 @@ function Homepage() {
                 <meta name="discord:description" content="Browse our e-commerce website for an extensive collection of video
                 games. Find the perfect game on our product page, featuring top-rated titles and unbeatable deals. About
                 this game"/>
-                <meta name="discord:image" content="../../../assets/images/banner/main.jpg"/>
+                <meta name="discord:image" content={Main}/>
             </Helmet>
             <div className='flex flex-col'>
                 <div className='flex items-center mt-4 mb-10'>
