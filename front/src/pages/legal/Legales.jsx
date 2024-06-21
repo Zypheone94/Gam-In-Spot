34d9f36003e64072cdf98b5bc225d3b33276ba02
Maxime Durand <maximerene.durand.pro@gmail.com>
2024-06-21 18:41:11 +0200

    TEST V2

--- front/src/pages/legal/Legales.jsx
+++ front/src/pages/legal/Legales.jsx
@@ -34,7 +34,7 @@ const Legales = () => {
 
             <p className='my-4'>Lien vers le site de la CNIL pour le traitement des données SEO : <a href="https://www.cnil.fr/fr/reglement-europeen-protection-donnees">Lien</a></p>
 
-<p>SAIKOU ET MAXIME</p>
+<p>SAIKOU ET MAXIME test</p>
         </div>
 
     )
